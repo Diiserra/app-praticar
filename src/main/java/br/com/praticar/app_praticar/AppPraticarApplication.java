1574ec0b8d4f534d8938e47a09daa9e5091aa68f
Diego Lima <diiserras@gmail.com>
2025-07-01 09:43:45 -0300

    Projeto Spring Boot inicializado

---
+++ src/main/java/br/com/praticar/app_praticar/AppPraticarApplication.java
@@ -0,0 +1,13 @@
+package br.com.praticar.app_praticar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPraticarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPraticarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/praticar/app_praticar/AppPraticarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.praticar.app_praticar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPraticarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
